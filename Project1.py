f1ea83e429694aefbe596d236de1b8fbfe25496d
Mohd Shadab <hukkacuk1@gmail.com>
2025-05-04 23:04:06 +0530

    updated with file handling

--- Project1.py
+++ Project1.py
@@ -1,4 +1,16 @@
-student_list= ["Mohd Shadab", "Rohit Sharma", "Shalini Arora", "Asheesh Chanchlani", "Arpita", "Shadab Khan", "Arpita Singh","Arun Jaiswal", "Kajal", "Arpita Sharma"]
+import os
+def search(a,lst):
+    new_list =set()
+    if a in lst:
+        return lst.index(a)
+    else:
+        for i in a.split():
+            for l,j in enumerate(lst):
+                for k in j.split():
+                    if i ==k:
+                        new_list.add(l)
+        return list(new_list)
+
 
 def search(a,lst):
     new_list =set()
@@ -12,6 +24,7 @@ def search(a,lst):
                         new_list.add(l)
         return list(new_list)
 
+
 print("-"*60)
 print("   *********WELCOME TO STUDENT MANAGEMENT SYSTEM*********")
 print("-"*60)
@@ -24,20 +37,29 @@ print("5. Exit")
 print("-"*60)
 
 while True:
-    n =(input("Enter your choice:"))
+    n=input("Enter your choice:")
     print()
     match n:
-        case "1":
-            for i,j in enumerate(student_list):
-                print(i,j)
+        case "1": 
+            if os.path.exists("data_file.txt"):
+                fileobj = open("data_file.txt", "r")
+                names = fileobj.read()
+                print(names)
+            else:
+                print("No file exists")
+            
             print()
         case "2":
             repeat = "y"
             while repeat=="y":
-                name = str.title(str.strip(input("Enter name of student:")))
-                student_list.append(name)
+                name = str.title(str.strip(input("Enter name of student:")))+"\n"
+                #student_list.append(name)
+                fileobj = open("data_file.txt", "a")
+                fileobj.write(name+"\n")
+                fileobj.close()
                 print("Name is added successfully..")
                 repeat = str.lower(input("DO you want to add more names of student (y/n):"))
+
                 while repeat != "n" and repeat != "y":
                     repeat= str.lower(input("Incorrect input, press only 'y' to add more names and 'n' for not:"))
                     if repeat == "n" or repeat == "y":
@@ -48,30 +70,29 @@ while True:
                     continue
             print()
         case "3":
-            name=str.title(str.strip(input("Enter name to delete:")))
-            if name in student_list:
-                student_list.remove(name)
-                print("Student name is successfully deleted..")
-            else:
-                index = search(name, student_list)
-                if len(index)==0:
-                    print("Name is not found in the list")
+            name=str.title(str.strip(input("Enter name to delete:")))+"\n"
+            if os.path.exists("data_file.txt"):
+                with open("data_file.txt", "r") as fileobj:
+                    student_list = fileobj.readlines()
+                    print(student_list)
+                for i in student_list:
+                    if i == name:
+                        student_list.remove(i)
+                        with open("data_file.txt", "w") as fileobj:
+                            fileobj.writelines(student_list)
+                        print("Name is removed")
+                        break
                 else:
-                    print("Exact match is not found but similar names is/are:")
-                    for i in index:
-                        print(f"{i}. {student_list[i]}")
-                    value=input("Enter index value of above name to delete or type 'cancel to cancel the request")
-                    while value!='cancel':
-                        if value.isdigit() ==True:
-                            del student_list[int(value)]
-                            break
-                        value=input("Incorrect input, enter index value of above name to delete or type 'cancel to cancel the request")
-                        if value== 'cancel':
-                            break
+                    print("Name is not in the list")
+                                
+            else:
+                print("File does not exist")
             print()
         case "4":
-            name=str.title(str.strip(input("Enter name to search:")))
-            index= search(name, student_list)
+            name=str.title(str.strip(input("Enter name to search:")))+'\n'
+            with open("data_file.txt", "r") as fileobj:
+                student_list = fileobj.readlines()
+            index=search(name, student_list)
             if type(index)==int:
                 print("Name is found in the list at index:",index)
             else:
@@ -81,7 +102,7 @@ while True:
                     print("Exact match is not found but similar names is/are:")
                     for i in index:
                         print(student_list[i])
-            print()
+            print()  
         case "5":
             exit()
         case _:
